efe2f4f3e486d0b7cd6192957d363af7006bdf1c
Martin Cowie <martinjcowie@gmail.com>
2026-07-13 15:48:47 +0100

    Configurable port number

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,29 @@ mod kafka;
 mod storage;
 
 use auth::TomlAuthenticator;
+use clap::Parser;
 use kafka::KafkaSendHandler;
 use libunftp::ServerBuilder;
 use slog::{o, Drain};
 use std::sync::Arc;
 use storage::MemStorage;
 
+#[derive(Parser)]
+struct Cli {
+    #[arg(short, long, default_value_t = 2121)]
+    port: u16,
+}
 
 #[tokio::main]
 async fn main() {
+    let cli = Cli::parse();
+
     // SLog ceremony
     let decorator = slog_term::TermDecorator::new().build();
     let drain = slog_async::Async::new(slog_term::FullFormat::new(decorator).build().fuse())
         .build()
         .fuse();
     let logger = slog::Logger::root(drain, o!());
-
-    let port: i16 = 2122;
 
     let authenticator =
         TomlAuthenticator::from_file("config.toml").expect("Failed to load config.toml");
@@ -32,7 +38,7 @@ async fn main() {
             .build()
             .expect("Failed to build FTP server");
 
-    let bind_address = format!("0.0.0.0:{}", port);
+    let bind_address = format!("0.0.0.0:{}", cli.port);
     slog::info!(logger, "FTP server listening on {}", bind_address);
     server.listen(bind_address).await.expect("Server error");
 }
